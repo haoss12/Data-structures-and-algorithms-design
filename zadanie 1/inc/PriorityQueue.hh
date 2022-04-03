f3b412b67bdfa74578f31822c3216630f3be70ea
haoss12 <73287767+haoss12@users.noreply.github.com>
2022-04-03 16:20:14 +0200

    3

--- zadanie 1/inc/PriorityQueue.hh
+++ zadanie 1/inc/PriorityQueue.hh
@@ -22,6 +22,48 @@ public:
 
     PriorityQueue(): head(nullptr), tail(nullptr), Size(0) {};
     int size() const {return Size;};
+    bool is_empty() const {return (head == nullptr);};
     T min() const;
     T removeMin();
+    void enqueue(const T & _data, const int & _priority);
 };
+
+template <typename T>
+void PriorityQueue<T>::enqueue(const T & _data, const int & _priority)
+{
+    Node *new_node = new Node(_data, _priority, nullptr, nullptr);
+    Node *temp = nullptr;   //wezel tymczasowy do poruszania sie po elementach
+
+    if (head == nullptr)    //pierwszy element do pustej kolejki
+    {
+        head = new_node;   
+        tail = new_node;
+        Size++;
+        return;
+    }
+    else                    //pozostale przypadki
+    {
+        temp = head;        //zaczynamy poszukiwania od pierwszego elementu
+
+        while (temp != nullptr && _priority > temp->priority)
+        {                       //poruszamy sie po strukturze w poszukiwaniu
+            temp = temp->prev;  //miejsca do umieszczenia nowego węzła
+        }                       //dopoki nie natrafimy na koniec kolejki
+        
+        if (temp == nullptr)    //przypadek gdy natrafimy na koniec i dodajemy
+        {                       //nowy element na tyl kolejki
+            tail->prev = new_node; 
+            new_node->next = tail;
+            new_node->prev = nullptr; //bo nie ma dalszych elementów
+            tail = new_node;    //nowy element staje sie nowym "ogonem"
+        }
+        
+    }
+    Size++;
+}
+
+template <typename T>
+T PriorityQueue<T>::min() const
+{
+    return head->data;
+}
--- zadanie 1/src/main.cpp
+++ zadanie 1/src/main.cpp
@@ -4,6 +4,11 @@
 int main() {
     PriorityQueue <int> a;
 
+    a.enqueue(8, 1);
+    a.enqueue(4, 2);
+    a.enqueue(4, 5);
+
+    std::cout << a.min() << std::endl;
     std::cout << a.size() << std::endl;
     return 0;
 }
